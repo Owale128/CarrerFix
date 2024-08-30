17214aa6c103f57e8f82acbc3582d664adf1792c
Owale128 <Christian.buaka@hotmail.com>
2024-08-30 14:56:58 +0200

    Added loading in adPage

--- src/pages/AdPage.tsx
+++ src/pages/AdPage.tsx
@@ -6,27 +6,27 @@ import { ExtendedAdDetails } from "../components/ExtendedAdDetails";
 
 export const AdPage = () => {
   const { id } = useParams<{ id: string }>();
-
   const [ad, setAd] = useState<IJobAd>();
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     if (id) {
       const getAdData = async (id: string) => {
         try {
           const data = await getAd(id);
-
           setAd(data);
-
           console.log("data retreived: ", data);
         } catch (error) {
           console.error("No data found", error);
+        } finally {
+          setIsLoading(false)
         }
       };
 
       getAdData(id);
     }
   }, [id]);
-
+        if(isLoading) return <h2>...Laddar...</h2>
   return (
     <>
       <ExtendedAdDetails ad={ad}></ExtendedAdDetails>
