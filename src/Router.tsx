32c14909f6bf030e34cb92a8d2649df674712c37
Owale128 <Christian.buaka@hotmail.com>
2024-08-30 12:43:01 +0200

    Added a fallback/spinner for AdPage

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import { Home } from "./pages/Home";
-
 import { AdPage } from "./pages/AdPage";
 import AboutPage from "./pages/AboutPage";
 import { AdsPage } from "./pages/AdsPage";
 import NotFound from "./pages/NotFound";
+import { Suspense } from "react";
+import Spinner from "./components/Spinner";
 
 export const router = createBrowserRouter([
   {
@@ -22,11 +23,19 @@ export const router = createBrowserRouter([
       },
       {
         path: "/ads",
-        element: <AdsPage />,
+        element: (
+          <Suspense fallback={<Spinner />}>
+          <AdsPage />
+          </Suspense>
+        ),
       },
       {
         path: "/ad/:id",
-        element: <AdPage />,
+        element: ( 
+        <Suspense fallback={<Spinner />}>
+        <AdPage />
+        </Suspense>
+        ),
       },
     ],
     errorElement: <NotFound />
