48b672df6f22f475652714890f008610d796d55f
Niccar95 <niccar.95@outlook.com>
2024-09-01 19:18:24 +0200

    input clearing after search doesn't affect pagination anymore

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -4,7 +4,7 @@ import {
 } from "@digi/arbetsformedlingen";
 import { DigiFormInputSearch } from "@digi/arbetsformedlingen-react";
 import { DigiFormInputSearchCustomEvent } from "@digi/arbetsformedlingen/dist/types/components";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { SearchTextContext } from "../contexts/SearchTextContext";
 
@@ -15,29 +15,31 @@ interface ISearchForm {
 export const SearchForm = ({ getAdData }: ISearchForm) => {
   const navigate = useNavigate();
 
-  const { searchText, setSearchText } = useContext(SearchTextContext);
+  const { setSearchText } = useContext(SearchTextContext);
+  const [inputValue, setInputValue] = useState("");
 
   const offset = 0;
   const limit = 10;
 
   const handleSearch = (e: DigiFormInputSearchCustomEvent<string>) => {
     e.preventDefault();
-    if (searchText.trim() !== "") {
-      getAdData(searchText, offset, limit);
+    if (inputValue.trim() !== "") {
+      setSearchText(inputValue);
+      getAdData(inputValue, offset, limit);
+      setInputValue("");
       navigate("/ads");
     }
   };
 
   return (
-    <DigiFormInputSearch 
+    <DigiFormInputSearch
       afLabel="Sök Jobb"
       afVariation={FormInputSearchVariation.MEDIUM}
       afType={FormInputType.SEARCH}
       afButtonText="Sök"
       onAfOnSubmitSearch={handleSearch}
-      onAfOnChange={(e) => setSearchText(e.target.value)}
-      value={searchText}
+      onAfOnChange={(e) => setInputValue(e.target.value)}
+      value={inputValue}
     ></DigiFormInputSearch>
   );
 };
-
