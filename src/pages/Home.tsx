4a9fc8e251c014eeac0a82c5f5697c15bf9bb24b
Niccar95 <niccar.95@outlook.com>
2024-09-03 21:55:53 +0200

    Began presentation of latest ads on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,87 +1,61 @@
-import { DigiInfoCard } from "@digi/arbetsformedlingen-react";
+import {
+  DigiLayoutBlock,
+  DigiTypography,
+  DigiTypographyTime,
+} from "@digi/arbetsformedlingen-react";
 import { SearchForm } from "../components/SearchForm";
 import { useAds } from "../hooks/useAds";
 import {
-  InfoCardHeadingLevel,
-  InfoCardSize,
-  InfoCardType,
-  InfoCardVariation,
+  LayoutBlockContainer,
+  LayoutBlockVariation,
+  TypographyTimeVariation,
 } from "@digi/arbetsformedlingen";
 import lunch1 from "../assets/lunch1.png";
 import { JobAdsContext } from "../contexts/JobAdsContext";
 import { useContext } from "react";
 import "../sass/infoCard.scss";
 import "../sass/homeImg.scss";
+import { NavLink } from "react-router-dom";
 
 export const Home = () => {
   const [getAdData] = useAds();
   const { jobAds } = useContext(JobAdsContext);
 
-  const latestAds = [...jobAds].slice(0, 9);
+  const latestAds = [...jobAds].slice(0, 3);
 
   console.log(latestAds);
 
   return (
     <>
       <SearchForm getAdData={getAdData} />
+      <h3>Annonser baserade på dina senaste sökningar</h3>
       <div className="info-cards-container">
-        <DigiInfoCard
-          className="infoCard"
-          afHeading="Jag är ett infokort"
-          afHeadingLevel={InfoCardHeadingLevel.H2}
-          afType={InfoCardType.TIP}
-          afLinkHref="Frivillig länk"
-          afLinkText="Sök NU"
-          afVariation={InfoCardVariation.PRIMARY}
-          afSize={InfoCardSize.STANDARD}
-        >
-          <p>
-            Tjoooo. Det här är bara ord för att illustrera hur det ser ut med
-            text inuti. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Suspendisse commodo egestas elit in consequat. Proin in ex
-            consectetur, laoreet augue sit amet, malesuada tellus.
-          </p>
-        </DigiInfoCard>
-        <DigiInfoCard
-          className="infoCard"
-          afHeading="Jag är ett infokort"
-          afHeadingLevel={InfoCardHeadingLevel.H2}
-          afType={InfoCardType.TIP}
-          afLinkHref="Frivillig länk"
-          afLinkText="Sök NU"
-          afVariation={InfoCardVariation.PRIMARY}
-          afSize={InfoCardSize.STANDARD}
-        >
-          <p>
-            Det här är bara ord för att illustrera hur det ser ut med text
-            inuti. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Suspendisse commodo egestas elit in consequat. Proin in ex
-            consectetur, laoreet augue sit amet, malesuada tellus.
-          </p>
-        </DigiInfoCard>
-        <DigiInfoCard
-          className="infoCard"
-          afHeading="Jag är ett infokort"
-          afHeadingLevel={InfoCardHeadingLevel.H2}
-          afType={InfoCardType.TIP}
-          afLinkHref="Frivillig länk"
-          afLinkText="Sök NU"
-          afVariation={InfoCardVariation.PRIMARY}
-          afSize={InfoCardSize.STANDARD}
-        >
-          <p>
-            Det här är bara ord för att illustrera hur det ser ut med text
-            inuti. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Suspendisse commodo egestas elit in consequat. Proin in ex
-            consectetur, laoreet augue sit amet, malesuada tellus.
-          </p>
-        </DigiInfoCard>
-
-        {/*
         {latestAds.map((ad) => (
-          <p>{ad.headline}</p>
+          <DigiLayoutBlock
+            key={ad.id}
+            afVariation={LayoutBlockVariation.PRIMARY}
+            afContainer={LayoutBlockContainer.NONE}
+            className="latest-ads-card"
+          >
+            <DigiTypography className="b">
+              <h3>{ad.headline}</h3>
+
+              <blockquote>
+                <NavLink to={`/ad/${ad.id}`} className="card-link">
+                  <p style={{ textDecoration: "underline", color: "blue" }}>
+                    Läs mer...
+                  </p>
+                </NavLink>
+              </blockquote>
+
+              <DigiTypographyTime
+                style={{ color: "black" }}
+                afVariation={TypographyTimeVariation.DISTANCE}
+                afDateTime={ad.publication_date}
+              ></DigiTypographyTime>
+            </DigiTypography>
+          </DigiLayoutBlock>
         ))}
-        */}
       </div>
       <div className="image-container">
         <img src={lunch1} alt="Lunch 1" className="lunchImg" />
