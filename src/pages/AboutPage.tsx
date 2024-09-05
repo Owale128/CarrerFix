a905c49500f7f7aee6a3d5122e45bb9e17159d81
LaNtZ445 <jacoblimparlantz@gmail.com>
2024-09-05 13:10:13 +0200

    added sections to about

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,21 +1,54 @@
-import RetroWave from "../assets/retro.png";
+import "../sass/AboutPage.scss";
+import { DigiButton } from "@digi/arbetsformedlingen-react";
+import { ButtonSize, ButtonVariation } from "@digi/arbetsformedlingen";
+import { useNavigate } from "react-router-dom";
 
 export const AboutPage = () => {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/");
+  };
+
   return (
-    <div style={{ height: "54vh" }}>
-      <img src={RetroWave} alt="About Us" className="aboutUsImage"/>
-      <h1>Om Vårt Projekt</h1>
-      <p>
-        Vi är en grupp studenter som har utvecklat en webbapplikation med React
-        som efterliknar Arbetsförmedlingens officiella webbplats. Vårt mål var
-        att skapa en realistisk och användarvänlig plattform för att lära oss
-        mer om webbutveckling och React.
-      </p>
-      <p>
-        Vi har designat en layout som återskapar Arbetsförmedlingens design,
-        implementerat funktioner som sökfunktioner och formulär, samt testat
-        och förbättrat allt för att säkerställa att det fungerar som det ska.
-      </p>
+    <div className="aboutPage">
+      <section className="topSection">
+        <div className="topContent">
+          <h1>Om Vårt Projekt</h1>
+          <p>
+            Vi är en grupp studenter som har utvecklat en webbapplikation med
+            React som efterliknar Arbetsförmedlingens officiella webbplats. Vårt
+            mål var att skapa en realistisk och användarvänlig plattform för att
+            lära oss mer om webbutveckling och React.
+          </p>
+        </div>
+      </section>
+      <section className="middleSection">
+        <div className="middleContent">
+          <h1>jadu nått ska vara här</h1>
+        </div>
+      </section>
+      <section className="bottomSection">
+        <div className="bottomContent">
+          <h1>
+            Hitta <span>drömjobbet</span> som tar din karriär till nästa nivå!
+          </h1>
+          <p>
+            Utforska dina karriärmöjligheter med oss! Vi erbjuder ett brett
+            utbud av spännande jobbmöjligheter som kan passa just dina
+            färdigheter och intressen. Ta första steget mot din drömkarriär
+            idag!
+          </p>
+          <DigiButton
+            afSize={ButtonSize.LARGE}
+            afVariation={ButtonVariation.PRIMARY}
+            afFullWidth={false}
+            onClick={handleButtonClick}
+          >
+            Sök jobb
+          </DigiButton>
+        </div>
+      </section>
     </div>
   );
 };
