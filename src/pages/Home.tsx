8d77e70e7aa6c34d0d5b30f4e0fa6726e025e3e5
Niccar95 <niccar.95@outlook.com>
2024-09-05 16:05:28 +0200

    banner styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
 import { SearchForm } from "../components/SearchForm";
 import { useAds } from "../hooks/useAds";
-import worker from "../assets/construction-worker-569126.jpg";
 import ShowRecentAds from "../components/ShowRecentAds";
 import SuggestedAds from "../components/SuggestedAds";
+import {
+  DigiButton,
+  DigiIconArrowDown,
+  DigiLayoutBlock,
+  DigiTypography,
+} from "@digi/arbetsformedlingen-react";
+import {
+  ButtonSize,
+  ButtonVariation,
+  LayoutBlockContainer,
+  LayoutBlockVariation,
+} from "@digi/arbetsformedlingen";
 
 export const Home = () => {
   const [getAdData] = useAds();
@@ -12,11 +23,28 @@ export const Home = () => {
       <SearchForm getAdData={getAdData} />
       <SuggestedAds />
       <div className="image-container">
-        <img src={worker} alt="worker" className="workerImg" />
+        <DigiLayoutBlock
+          afVariation={LayoutBlockVariation.TRANSPARENT}
+          afContainer={LayoutBlockContainer.NONE}
+          className="info"
+        >
+          <DigiTypography>
+            <blockquote>
+              <h1>Titta på våra senaste annonser</h1>
+              <DigiButton
+                afSize={ButtonSize.LARGE}
+                afVariation={ButtonVariation.SECONDARY}
+                afFullWidth={false}
+              >
+                Kom igång
+                <DigiIconArrowDown slot="icon-secondary" />
+              </DigiButton>
+            </blockquote>
+          </DigiTypography>
+        </DigiLayoutBlock>
       </div>
-      <div>
-        <ShowRecentAds />
-      </div>
+
+      <ShowRecentAds />
     </>
   );
 };
