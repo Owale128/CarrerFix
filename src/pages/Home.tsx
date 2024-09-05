b5857c6d00a0feee6294f51e44b965fbbafb996d
Niccar95 <niccar.95@outlook.com>
2024-09-05 12:48:59 +0200

    fixed image alignment and changed image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { SearchForm } from "../components/SearchForm";
 import { useAds } from "../hooks/useAds";
-import lunch1 from "../assets/lunch1.png";
+import worker from "../assets/construction-worker-569126.jpg";
 import ShowRecentAds from "../components/ShowRecentAds";
 import SuggestedAds from "../components/SuggestedAds";
 
@@ -10,11 +10,9 @@ export const Home = () => {
   return (
     <>
       <SearchForm getAdData={getAdData} />
-      <div>
-        <SuggestedAds />
-      </div>
+      <SuggestedAds />
       <div className="image-container">
-        <img src={lunch1} alt="Lunch 1" className="lunchImg" />
+        <img src={worker} alt="worker" className="workerImg" />
       </div>
       <div>
         <ShowRecentAds />
